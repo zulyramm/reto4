bc1da6175625a2e8784f9c4f9e95a6216582aca1
zulyramm <zulyramm@gmail.com>
2022-12-26 00:31:36 -0500

    swiper- js

--- reto 4-mary/js/index.js
+++ reto 4-mary/js/index.js
@@ -1,24 +1,27 @@
-//'use strict';
-import characters from "./modules/characters.js";
-
-document.addEventListener('DOMContentLoaded',documentReady);
-
+'use strict';
 import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.browser.min.js'
 
 new Swiper('.swiper', {
-  autoplay: {
-    delay: 5000
-  },
-  loop: true,
-  speed: 500,
-  pagination: {
-    el: '.swiper-pagination',
-  },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  }
+    autoplay: {
+        delay: 5000
+    },
+    loop: true,
+    speed: 500,
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    }
 });
-const documentReady = () =>{
+
+import characters from "./modules/characters.js";
+
+const documentReady = () => {
     characters();
 };
+
+document.addEventListener('DOMContentLoaded', documentReady);
+
+
