53146a1fa5118ee763f1fea23e63597b68527914
zulyramm <zulyramm@gmail.com>
2022-12-25 23:59:30 -0500

    swiper

--- reto 4-mary/js/index.js
+++ reto 4-mary/js/index.js
@@ -1,8 +1,24 @@
-'use strict';
+//'use strict';
 import characters from "./modules/characters.js";
 
+document.addEventListener('DOMContentLoaded',documentReady);
+
+import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.browser.min.js'
+
+new Swiper('.swiper', {
+  autoplay: {
+    delay: 5000
+  },
+  loop: true,
+  speed: 500,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+});
 const documentReady = () =>{
     characters();
 };
-
-document.addEventListener('DOMContentLoaded',documentReady);
